4921798744fc72de1f710b8190b90b7e84b1831f
Antuan <tony96314@hotmail.com>
2022-06-25 04:50:10 -0700

    stim: Add Base class interface for stimuli generation
    All generated stimuli will inherited from this class.

---
+++ Include/Stimuli.h
@@ -0,0 +1,194 @@
+/**
+ * @file StimBase.h
+ *
+ * @brief Declares a base class for all stimuli's generation.
+ * @author	Ing. Antonio A. Iznaga Marín.
+ * @date	11/06/2017
+ */
+#ifndef STIMBASE_H
+#define STIMBASE_H
+
+#include "ASGMath.h"
+
+/**
+ * @brief Auditory Stimulator Generator API namespace.
+ */
+namespace ASG
+{
+	///Forward declaration for generated stimuli parameters class.
+	class StimParameters;
+/**
+ * @brief Base class for stimuli generation.
+ *
+ * @remarks This method is an interface to be implemented by all generated
+ * stimuli.
+ */
+class Stimuli
+{
+public:
+	/**
+	 * @fn Stimuli::Stimuli();
+	 *
+	 * @brief Stimuli constructor. This class is an interface for each
+	 * stimuli to be generated or reproduced by the core APP.
+	 */
+	Stimuli();
+
+	/**
+	 * @fn virtual ~Stimuli();
+	 *
+	 * @brief virtual destructor
+	 */
+	virtual ~Stimuli();
+
+	/**
+	 * @fn virtual std::size_t generate(std::float32_t *sBuffer, std::size_t size,
+	 * 									std::int32_t &sync) = 0;
+	 *
+	 * @brief Generation method. Each stimuli will generate its own based on this
+	 * 			simple interface.
+	 * @param [out] sBuffer: Buffer to store generated samples.
+	 * @param size Amount of samples to be generated.
+	 * @param [out] sync Index marking sample inside Buffer for synchronism mark.
+	 * @return Amount of samples generated. Zero for error on generation method.
+	 */
+	virtual std::size_t generate(std::float32_t *sBuffer, std::size_t size,
+								 std::int32_t &sync) = 0;
+
+	/**
+	 * @fn 	virtual std::size_t reproduce(const std::float32_t* const srcBuffer,
+	 * 										std::float32_t *dstBuffer,
+	 * 										std::size_t size, std::int32_t &sync) = 0;
+	 *
+	 * @brief Reproduction method. Some stimuli cannot be generated but instead
+	 * 			will be reproduced from previuosly stored samples on memory using
+	 * 			this common interfase.
+	 * @param [in] srcBuffer: Buffer storing previously generated samples to be reproduced.
+	 * @param [out] dstBuffer: Buffer to store samples to be reproduced after being properly
+	 * 							normalized to target system.
+	 * @param size	Amount of samples to be reproduced.
+	 * @param [out] sync Index marking sample inside Buffer for sinchronism mark.
+	 * @return Amount of samples reproduced. Zero for error.
+	 */
+	virtual std::size_t reproduce(const std::float32_t* const srcBuffer,
+								  std::float32_t *dstBuffer,
+								  std::size_t size, std::int32_t &sync) = 0;
+
+	/**
+	 * @fn bool configure(ASG::StimParameters *typeParameters,
+	 * 						const std::uint16_t typeSize) = 0;
+	 *
+	 * @brief Configuration method. Each stimuli will be generated after being
+	 * 			properly configured by type parameters.
+	 * @param typeParameters Data structure tailored for each stimuli to be generated.
+	 * @param typeSize Amount of bytes for data structure holding each parameter
+	 * 					type.
+	 * @return True on success, false if it fails.
+	 */
+	virtual bool configure(ASG::StimParameters *typeParameters,
+						   const std::uint16_t typeSize) = 0;
+
+	/**
+	 * @fn bool configure(ASG::StimParameters *typeParameters,
+	 * 						const std::uint16_t typeSize,
+	 * 						const std::uint8_t earSide) = 0;
+	 *
+	 * @brief Overload configuration method.
+	 * @param typeParameters Data structure tailored for each stimuli to be generated.
+	 * @param typeSize Amount of bytes for data structure holding each parameter
+	 * 					type.
+	 * @param earSide
+	 * @return True on success, false if it fails.
+	 */
+	virtual bool configure(ASG::StimParameters *typeParameters,
+						   const std::uint16_t typeSize,
+						   const std::uint8_t earSide) = 0;
+
+	/**
+	 * @fn std::float32_t getPeriod_secs() const = 0;
+	 *
+	 * @brief Getted method for
+	 * @return
+	 */
+	virtual std::float32_t getPeriod_secs() const = 0;
+
+	/**
+	 * @fn std::uint32_t getPosition() const;
+	 *
+	 * @brief
+	 * @return
+	 */
+	std::uint32_t getPosition() const;
+
+	/**
+	 * @fn bool setPosition(std::uint32_t position);
+	 *
+	 * @brief
+	 * @param position
+	 * @return
+	 */
+	virtual bool setPosition(std::uint32_t position);
+
+	/**
+	 * @fn bool resetPosition();
+	 *
+	 * @brief
+	 * @return
+	 */
+	virtual bool resetPosition();
+
+	/**
+	 * @fn std::float32_t getSamplingFrequency() const;
+	 *
+	 * @brief
+	 * @return
+	 */
+	std::float32_t getSamplingFrequency() const;
+
+	/**
+	 * @fn bool setSamplingFrequency(std::float32_t samplingFrequency);
+	 *
+	 * @brief Setter method for sampling frecuency for stimuli generation.
+	 * @param samplingFrequency IEEE-754 value to be set as sampling frecuency
+	 * 			for stimuli generation.
+	 * @return True on success, false if it fails (ie negative value)
+	 */
+	virtual bool setSamplingFrequency(std::float32_t samplingFrequency);
+
+	/**
+	 * @fn std::uint32_t leastCommonMultiple(std::uint32_t numberA,
+	 * 										std::uint32_t numberB);
+	 *
+	 * @brief
+	 * @param numA
+	 * @param numB
+	 * @return
+	 */
+	std::uint32_t leastCommonMultiple(std::uint32_t numA, std::uint32_t numB);
+
+protected:
+	/**
+	 * @brief Last generated sample Index for next call.
+	 * @remarks It holds the position index based for the last generated sample.
+	 */
+	std::uint32_t position_;
+
+	/**
+	 * @brief The sampling frequency for the generated stimuli.
+	 */
+	std::float32_t samplingFrequency_;
+
+	/**
+	 * @brief Constant for single value sample.
+	 */
+	static const std::size_t SINGLE_SAMPLE = 1;
+
+private:
+	/**
+	 * @brief Prevents duplication of these object.
+	 */
+	E_DISABLE_COPY(Stimuli);
+
+};
+}///end of namespace
+#endif /*STIMBASE_H*/
---
+++ Source/Stimuli.cpp
@@ -0,0 +1,9 @@
+/**
+ * @file Stimuli.cpp
+ *
+ * @brief Defines a base class for the implementation of all stimuli's generation.
+ * @author	Ing. Antonio A. Iznaga Marín.
+ * @date	11/06/2017
+ */
+
+#include "Stimuli.h"
